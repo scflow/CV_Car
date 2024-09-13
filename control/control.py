daa94878ec4f077757b8b8a3e928cb2dcc740646
one <hyperslip@outlook.com>
2024-09-13 13:33:22 +0800

    control部分代码更新

--- control/control.py
+++ control/control.py
@@ -11,25 +11,30 @@ class Motor:
         self.pi.set_PWM_frequency(self.pin, 50)
         self.pi.set_PWM_range(self.pin, 40000)
 
-    def speed(self, speed):
-        self.pi.set_PWM_dutycycle(self.pin, speed)
+    def set_speed(self, speed):
+        """
+        speed范围为0-100, speed会计算并转为对应pwm占空比值
+        :speed 0 为停止， 6 电机即可启动
+        """
+        pwm_dutycycle = (100 - speed) * 100
+        self.pi.set_PWM_dutycycle(self.pin, pwm_dutycycle)
 
 
 class Servo:
-    def __init__(self, name, pin):
+    def __init__(self, name, pin, frequency=50, pwm_range=1000):
         self.pi = pigpio.pi()
         self.name = name
         self.pin = pin
+        self.range = pwm_range
         self.pi.set_mode(self.pin, pigpio.OUTPUT)
-        self.pi.set_PWM_frequency(self.pin, 200)
-        self.pi.set_PWM_range(self.pin, 40000)
+        self.pi.set_PWM_frequency(self.pin, frequency)
+        self.pi.set_PWM_range(self.pin, self.range)
 
-    def angle(self, angle):
-        self.pi.set_PWM_dutycycle(self.pin, angleToDutyCycle(angle))
-
+    def set_angle(self, angle):
+        self.pi.set_PWM_dutycycle(self.pin, angleToDutyCycle(angle) * self.range / 100)
 
 def angleToDutyCycle(angle):
-    return 2.5 + (angle / 180.0) * 10
+    return (2.5 + (angle / 180.0) * 10) * 1
 
 
 def pigpio_start():
@@ -47,5 +52,5 @@ close_io = "sudo killall pigpiod"
 
 motor = Motor()
 servo = Servo('servo', 12)
-gimbal_x = Servo('gimbal_x', 12)
-gimbal_y = Servo('gimbal_y', 12)
+gimbal_x = Servo('gimbal_x', 22)
+gimbal_y = Servo('gimbal_y', 23)
